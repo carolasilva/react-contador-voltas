61dcd6c82f27e3a7c3120bf035e64a0c6829835d
Carolina Alves <carol.alves99@live.com>
2020-04-10 19:59:03 -0300

    Tempo médio de volta funcionando

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ const MostraVoltas = (props) => {
 }
 
 const MostraTempo = (props) => {
+  const tempo = props.tempo;
+  const minutos = Math.round(tempo / 60);
+  const segundos = tempo % 60;
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos;
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos;
   return (
     <p>
-      {props.tempo}<br />
+      {`${minutosStr}:${segundosStr}`}<br />
       Tempo médio por volta
     </p>
   );
@@ -21,13 +26,31 @@ const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 function App() {
   const [numVoltas, setNumVoltas] = useState(0);
+  const [running, setRunning] = useState(false);
   const [tempo, setTempo] = useState(0);
 
+
+
   useEffect(() => {
-    setInterval(() => {
-      console.log('chamou');
-    }, 1000)
-  }, []);
+    let timer = null;
+    
+    if(running) {
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    
+    return () => {
+      if(timer) {
+        clearInterval(timer);
+      }
+    }
+
+  }, [running]);
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  }
 
   const increment = () => {
     setNumVoltas(numVoltas + 1);
@@ -37,14 +60,22 @@ function App() {
     setNumVoltas(numVoltas - 1);
   }
 
+  const reset = () => {
+    setNumVoltas(0);
+    setTempo(0);
+  }
+
   return ( 
     <div className='App'>
       <MostraVoltas voltas={numVoltas} />
       <Button text='+' onClick={increment} />
       <Button text='-' onClick={decrement} />
-      <MostraTempo tempo={tempo} />
-      <Button text='Iniciar' />
-      <Button text='Reiniciar' />
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo/numVoltas)} />
+      }
+      <Button text='Iniciar' onClick={toggleRunning} />
+      <Button onClick={reset} text='Reiniciar' />
     </div>
   );
 }
